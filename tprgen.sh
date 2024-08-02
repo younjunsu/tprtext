bf9d0524b3aef4391dcdff86a934871ef33bfba6
younjunsu <junsu_youn@naver.com>
2024-08-02 10:42:22 +0900

    Update tprgen.sh

--- tprgen.sh
+++ tprgen.sh
@@ -13,3 +13,40 @@ function tprgen(){
     echo " -time YYYMMDDhh24:mi:ss YYYYYMMDDhh24:mi:ss"
     echo "-----------------------------"
 }
+
+
+function all_generator(){
+    echo ""
+}
+
+function time_generator(){
+    function arg_format(){
+        begin_time=$arg2
+        end_time=$arg3
+        
+        if [[ ! "$begin_time" =~ ^[0-9]{14}$ ]] || [[ ! "$end_time" =~ ^[0-9]{14}$ ]] 
+        then
+            echo "error"
+        fi
+    }
+    echo "gen"
+    arg_format
+
+}
+
+arg1=$1
+arg2=$2
+arg3=$3
+
+case $arg1 in
+    "-all")
+        all_generator
+    ;;
+    "-time")
+        time_generator
+    ;;
+    *)
+        echo "EXIT"
+        exit
+    ;;
+esac 
